e23a627a480ac473f33b9d74bc76902e05bf6a54
Jandrocr__ <jandristu7@gmail.com>
2023-04-26 10:38:42 +0200

    Filtros 2.0(TODOS REALIZADOS CORRECTAMENTE)

--- pagina-de-peliculas/src/components/peliculas/peliculas.js
+++ pagina-de-peliculas/src/components/peliculas/peliculas.js
@@ -5,9 +5,13 @@ import CartaPelicula from "./carta_pelicula";
 import { HeaderContext } from "../header/headerContext";
 
 const Peliculas = () => {
-    const {movieData} = useContext(HeaderContext);
     const {updateMovieData} = useContext(HeaderContext);
     const [peliculasFiltradas, setPeliculasFiltradas] = useState([]);
+    const [categoryFilter, setCategoryFilter] = useState('');
+    const [yearFilter, setYearFilter] = useState('');
+    const [durationFilter, setDurationFilter] = useState('');
+    const [sortOrder, setSortOrder] = useState('');
+
     // creamos el estado de peliculas
     const [peliculas, setPeliculas] = useState([]);
     const estado = "Loading..."
@@ -25,82 +29,105 @@ const Peliculas = () => {
             setPeliculas(peliculas);
             setPeliculasFiltradas(peliculas);
             updateMovieData(peliculas)
-            console.log(peliculas);
           };
           obtenerPeliculas();
 
     }, []);
 
-    const filtrar = () => {
-      var categoriaSeleccionada=document.getElementById("category").value;
-      var duracionSeleccionada=document.getElementById("duration").value;
-      var opcionOrdenarSeleccionada=document.getElementById("sort").value;
-
-      if(categoriaSeleccionada != "" && duracionSeleccionada != ""){
-        var listaFiltrada = [];
-        if(duracionSeleccionada == "90"){
-          listaFiltrada = movieData.filter(pelicula => parseInt(pelicula.runtime) < 90 && pelicula.genres.includes(categoriaSeleccionada));
-        }else if(duracionSeleccionada == "90-120"){
-          listaFiltrada = movieData.filter(pelicula => parseInt(pelicula.runtime) >= 90 && parseInt(pelicula.runtime) <= 120 && pelicula.genres.includes(categoriaSeleccionada));
-        }else{
-          listaFiltrada = movieData.filter(pelicula => parseInt(pelicula.runtime) > 120 && pelicula.genres.includes(categoriaSeleccionada));
+    useEffect(() => {
+      const filtrarPeliculas = () => {
+        let peliculasFiltradas = peliculas;
+        if (categoryFilter !== '') {
+          peliculasFiltradas = peliculasFiltradas.filter(pelicula => pelicula.genres.includes(categoryFilter));
         }
-        setPeliculasFiltradas(listaFiltrada);
-      }else if(categoriaSeleccionada == "" && duracionSeleccionada != ""){
-        filtrarPorDuracion(duracionSeleccionada);
-      }else if(categoriaSeleccionada != "" && duracionSeleccionada == ""){
-        filtrarPorCategoria(categoriaSeleccionada);
-      }else{
-        setPeliculasFiltradas(movieData);
-      }
+        if (yearFilter !== '') {
+          peliculasFiltradas = peliculasFiltradas.filter((pelicula) => {
+            const year = pelicula.release_date.slice(0, 4);
+            switch (yearFilter) {
+              case '2020s':
+                return year >= 2020 && year <= 2029;
+              case '2010s':
+                return year >= 2010 && year <= 2019;
+              case '2000s':
+                return year >= 2000 && year <= 2009;
+              case '1990s':
+                return year >= 1990 && year <= 1999;
+              case '1980s':
+                return year >= 1980 && year <= 1989;
+              case 'pre-1980s':
+                return year < 1980;
+              default:
+                return true;
+            }
+          });
+        }
+        if (durationFilter !== '') {
+          if (durationFilter === '90') {
+            peliculasFiltradas = peliculasFiltradas.filter(pelicula => parseInt(pelicula.runtime) < 90);
+          } else if (durationFilter === '90-120') {
+            peliculasFiltradas = peliculasFiltradas.filter(pelicula => parseInt(pelicula.runtime) >= 90 && parseInt(pelicula.runtime) <= 120);
+          } else if (durationFilter === '120') {
+            peliculasFiltradas = peliculasFiltradas.filter(pelicula => parseInt(pelicula.runtime) > 120);
+          }
+        }
+        if (sortOrder !== '') {
+          if (sortOrder === 'asc') {
+            peliculasFiltradas = peliculasFiltradas.sort((a, b) =>{
+              if(a.title < b.title) return -1;
+              else return 1;
+            });
+          } else if (sortOrder === 'desc') {
+            peliculasFiltradas = peliculasFiltradas.sort((a, b) => {
+              if(a.title > b.title) return -1;
+              else return 1;
+            });
+          }
+        }
+        setPeliculasFiltradas(peliculasFiltradas);
+      };
+    
+    filtrarPeliculas();
+    }, [categoryFilter, yearFilter, durationFilter, sortOrder, peliculas]);
+    
+    return (
+      <div className="container-peliculas">
+        <div className="filters peliculas">
+          <select id="category" name="category" onChange={(e) => setCategoryFilter(e.target.value)}>
+            <option value="">Filtrar por categoría</option>
+            <option value="Action">Acción</option>
+            <option value="Adventure">Aventura</option>
+            <option value="Drama">Drama</option>
+            <option value="Comedy">Comedia</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Western">Wéstern</option>
+            <option value="Romance">Romance</option>
+            <option value="Fantasy">Fantasía</option>
+            <option value="Horror">Horror</option>
+            <option value="Crime">Crimen</option>
+            <option value="Mystery">Misterio</option>
+          </select>
+          <select id="year" name="year" onChange={(e) => setYearFilter(e.target.value)}>
+            <option value="">Filtrar por año de lanzamiento</option>
+            <option value="2020s">2020s</option>
+            <option value="2010s">2010s</option>
+            <option value="2000s">2000s</option>
+            <option value="1990s">1990s</option>
+            <option value="1980s">1980s</option>
+            <option value="pre-1980s">pre-1980s</option>
+          </select>
+          <select id="duration" name="duration" onChange={(e) => setDurationFilter(e.target.value)}>
+            <option value="">Filtrar por duración</option>
+            <option value="90">Menos de 90 minutos</option>
+            <option value="90-120">Entre 90 y 120 minutos</option>
+            <option value="120">Más de 120 minutos</option>
+          </select>
+          <select id="sort" name="sort" onChange={(e) => setSortOrder(e.target.value)}>
+            <option value="">Ordenar por título</option>
+            <option value="asc">Ascendente</option>
+            <option value="desc">Descendente</option>
+          </select>
+        </div> 
 
-      if(opcionOrdenarSeleccionada != ""){
-        ordenarPeliculas(opcionOrdenarSeleccionada);
-      }
-    }
-
-    const filtrarPorCategoria = (categoria) => {
-      const listaFiltrada = movieData.filter(pelicula => pelicula.genres.includes(categoria));
-      setPeliculasFiltradas(listaFiltrada);
-    }
-
-    const filtrarPorDuracion = (duracion) => {
-      var listaFiltrada = [];
-      if(duracion == "90"){
-        listaFiltrada = movieData.filter(pelicula => parseInt(pelicula.runtime) < 90);
-      }else if(duracion == "90-120"){
-        listaFiltrada = movieData.filter(pelicula => parseInt(pelicula.runtime) >= 90 && parseInt(pelicula.runtime) <= 120);
-      }else if(duracion == "120"){
-        listaFiltrada = movieData.filter(pelicula => parseInt(pelicula.runtime) > 120);
-      }else{
-        listaFiltrada = movieData;
-      }
-      setPeliculasFiltradas(listaFiltrada);
-      
-    }
-
-    const ordenarPeliculas = (valorOrdenar) => {
-      var listaOrdenada=[];
-      if(valorOrdenar == "asc"){
-        listaOrdenada = peliculasFiltradas.sort((a, b) => {
-          if(a.title < b.title) return -1;
-          else return 1;
-        });
-      }else if(valorOrdenar == "desc"){
-        listaOrdenada = peliculasFiltradas.sort((a, b) => {
-          if(a.title > b.title) return -1;
-          else return 1;
-        });
-      }else{
-        listaOrdenada = peliculasFiltradas;
-      }
-      
-      console.log(listaOrdenada);
-      setPeliculasFiltradas(listaOrdenada);
-    }
-
-    const pintarPeliculasFiltradas = () => {
-      return (
         <div className="peliculas">
           {peliculasFiltradas.map((pelicula) => (
             <React.Fragment key={pelicula.id}>
@@ -110,59 +137,15 @@ const Peliculas = () => {
             </React.Fragment>
           ))}
           {
-            Peliculas.length == 0 && estado
+            peliculasFiltradas.length === 0 && estado
           }
         </div>
-      );
-    }
-
-    return (
-      <div className="container-peliculas">
-        <div className="filters peliculas">
-          <select id="category" name="category" onChange={filtrar}>
-            <option value="">Filtrar por categoría</option>
-            <option value="Action">Acción</option>
-            <option value="Adventure">Aventura</option>
-            <option value="Drama">Drama</option>
-            <option value="Comedy">Comedia</option>
-          </select>
-          <select id="year" name="year">
-            <option value="">Filtrar por año de lanzamiento</option>
-            <option value="2022">2022</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
-            <option value="2019">2019</option>
-          </select>
-          <select id="duration" name="duration" onChange={filtrar}>
-            <option value="">Filtrar por duración</option>
-            <option value="90">Menos de 90 minutos</option>
-            <option value="90-120">Entre 90 y 120 minutos</option>
-            <option value="120">Más de 120 minutos</option>
-          </select>
-          <select id="sort" name="sort" onChange={filtrar}>
-            <option value="">Ordenar por título</option>
-            <option value="asc">Ascendente</option>
-            <option value="desc">Descendente</option>
-          </select>
-        </div>
-
-        {pintarPeliculasFiltradas()}
         {peliculasFiltradas.length === 0 && 
-          <div className="peliculas">
-            {peliculas.map((pelicula) => (
-              <React.Fragment key={pelicula.id}>
-                {pelicula.poster && /^http/.test(pelicula.poster) && (
-                  <CartaPelicula pelicula={pelicula} />
-                )}
-              </React.Fragment>
-            ))}
-            {
-              Peliculas.length == 0 && estado
-            }
+          <div id='noPelis'>
+              <p>No se encuentran películas con los filtros especficados</p>
           </div>
         }
-      </div>
-      
+      </div>      
     );
 
 }
