d256ecce54d91a185d180b44d19ec12be39a885d
Jandrocr__ <jandristu7@gmail.com>
2023-05-24 21:56:31 +0200

    commit

--- pagina-de-peliculas/src/components/peliculas/detalle_pelicula.js
+++ pagina-de-peliculas/src/components/peliculas/detalle_pelicula.js
@@ -90,7 +90,7 @@ const DetallePelicula = () => {
     var nota3 = reviews.filter(rev => rev.valoracion === 3)
     var nota4 = reviews.filter(rev => rev.valoracion === 4)
     var nota5 = reviews.filter(rev => rev.valoracion === 5)
-    var notaMedia = ((1*nota1.length)+(2*nota2.length)+(3*nota3.length)+(4*nota4.length)+(5*nota5.length))/reviews.length;
+    var notaMedia = ((1 * nota1.length) + (2 * nota2.length) + (3 * nota3.length) + (4 * nota4.length) + (5 * nota5.length)) / reviews.length;
     const etiquetas = ['1', '2', '3', '4', '5'];
     const valores = [nota1.length, nota2.length, nota3.length, nota4.length, nota5.length];
     const data = {
@@ -104,7 +104,7 @@ const DetallePelicula = () => {
         },
       ],
     };
-  
+
     const options = {
       plugins: {
         legend: {
@@ -125,7 +125,7 @@ const DetallePelicula = () => {
         y: {
           display: false,
         },
-      },  
+      },
       animation: {
         duration: 0,
       },
@@ -135,8 +135,16 @@ const DetallePelicula = () => {
           right: 50,
         },
       },
+      tooltips: {
+        callbacks: {
+          label: function (tooltipItem) {
+            // Personaliza el contenido de las etiquetas aquí
+            return 'a: ' + tooltipItem.yLabel;
+          },
+        },
+      },
     };
-  
+
     return (
       <div id="grafico-detalle-padre">
         <legend id="legend-detalle">Valoración de los usuarios</legend>
@@ -330,20 +338,20 @@ const DetallePelicula = () => {
                     {genre.name}
                     {index !== parsedGenres.length - 1 && ", "}
                   </span>
-                ))}                
+                ))}
               </p>
             </div>
-            
+
             <p id="sinopsis-detalle">{pelicula.overview}</p>
             <div>
-            <div>
-              <BarChart />
+              <div>
+                <BarChart />
+              </div>
+
+
             </div>
 
-              
-            </div>
 
-            
           </div>
         </div>
         <div id="resena-form">
@@ -374,14 +382,14 @@ const DetallePelicula = () => {
             </div>
 
           </div>
-          
+
         </div>
         <div id="castYReviews">
           <div id="botonesCastYReviews">
             <button id="boton-cast-detalle" onClick={() => toggleCastYReviews("cast")}>Reparto</button>
             <button id="boton-reviews-detalle" onClick={() => toggleCastYReviews("reviews")}>Reseñas</button>
             <a target="_blank" href={`https://www.imdb.com/title/${pelicula.imdb_id}`}>
-                <svg id="svgimdb" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM21.3 229.2H21c.1-.1.2-.3.3-.4zM97 319.8H64V192h33zm113.2 0h-28.7v-86.4l-11.6 86.4h-20.6l-12.2-84.5v84.5h-29V192h42.8c3.3 19.8 6 39.9 8.7 59.9l7.6-59.9h43zm11.4 0V192h24.6c17.6 0 44.7-1.6 49 20.9 1.7 7.6 1.4 16.3 1.4 24.4 0 88.5 11.1 82.6-75 82.5zm160.9-29.2c0 15.7-2.4 30.9-22.2 30.9-9 0-15.2-3-20.9-9.8l-1.9 8.1h-29.8V192h31.7v41.7c6-6.5 12-9.2 20.9-9.2 21.4 0 22.2 12.8 22.2 30.1zM265 229.9c0-9.7 1.6-16-10.3-16v83.7c12.2.3 10.3-8.7 10.3-18.4zm85.5 26.1c0-5.4 1.1-12.7-6.2-12.7-6 0-4.9 8.9-4.9 12.7 0 .6-1.1 39.6 1.1 44.7.8 1.6 2.2 2.4 3.8 2.4 7.8 0 6.2-9 6.2-14.4z" fill="#ffdd00"></path></svg>
+              <svg id="svgimdb" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM21.3 229.2H21c.1-.1.2-.3.3-.4zM97 319.8H64V192h33zm113.2 0h-28.7v-86.4l-11.6 86.4h-20.6l-12.2-84.5v84.5h-29V192h42.8c3.3 19.8 6 39.9 8.7 59.9l7.6-59.9h43zm11.4 0V192h24.6c17.6 0 44.7-1.6 49 20.9 1.7 7.6 1.4 16.3 1.4 24.4 0 88.5 11.1 82.6-75 82.5zm160.9-29.2c0 15.7-2.4 30.9-22.2 30.9-9 0-15.2-3-20.9-9.8l-1.9 8.1h-29.8V192h31.7v41.7c6-6.5 12-9.2 20.9-9.2 21.4 0 22.2 12.8 22.2 30.1zM265 229.9c0-9.7 1.6-16-10.3-16v83.7c12.2.3 10.3-8.7 10.3-18.4zm85.5 26.1c0-5.4 1.1-12.7-6.2-12.7-6 0-4.9 8.9-4.9 12.7 0 .6-1.1 39.6 1.1 44.7.8 1.6 2.2 2.4 3.8 2.4 7.8 0 6.2-9 6.2-14.4z" fill="#ffdd00"></path></svg>
             </a>
           </div>
           <div id="castDetalle">
@@ -421,7 +429,7 @@ const DetallePelicula = () => {
                   <h5 id="titulo-reviews-h3"><span id="numero-reviews">{listaReviewsFiltrada.length}</span> reseñas</h5>
                   {listaReviewsFiltrada.slice(0, visibleReviews).map((review) => (
                     <div className="divReview" key={review.id} id={review.id}>
-                      
+
                       <div>
                         <div
                           className="divUsuarioReview"
@@ -432,42 +440,42 @@ const DetallePelicula = () => {
                         </div>
 
                         <div className="contenido-review-detalle">{review.contenido}</div>
-                          {review.valoracion === 1 ? (
-                            <div className="nota-comentario-detalle"><FontAwesomeIcon icon={faStar} /></div>
-                          ) : null}
+                        {review.valoracion === 1 ? (
+                          <div className="nota-comentario-detalle"><FontAwesomeIcon icon={faStar} /></div>
+                        ) : null}
                         {review.valoracion === 2 ? (
-                            <div className="nota-comentario-detalle">
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                            </div>
-                          ) : null}
+                          <div className="nota-comentario-detalle">
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                          </div>
+                        ) : null}
                         {review.valoracion === 3 ? (
-                            <div className="nota-comentario-detalle">
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                            </div>
-                          ) : null}
+                          <div className="nota-comentario-detalle">
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                          </div>
+                        ) : null}
                         {review.valoracion === 4 ? (
-                            <div className="nota-comentario-detalle">
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                            </div>
-                          ) : null}
+                          <div className="nota-comentario-detalle">
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                          </div>
+                        ) : null}
                         {review.valoracion === 5 ? (
-                            <div className="nota-comentario-detalle">
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                              <FontAwesomeIcon icon={faStar} />
-                            </div>
-                          ) : null}
-                        
+                          <div className="nota-comentario-detalle">
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                          </div>
+                        ) : null}
+
                         {review.id_usuario === userData.id && (
-                          <button onClick={() => eliminarReview(review.id)}>
+                          <button id="boton-eliminar-detalle" onClick={() => eliminarReview(review.id)}>
                             Eliminar reseña
                           </button>
                         )}
