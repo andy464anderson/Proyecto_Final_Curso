580d60795c2b25b80636815f33bbbd392175e923
HAIDER Ali <ali.haider@ext.soprasteria.com>
2023-06-07 19:20:33 +0200

    bootstrap agrgado

--- pagina-de-peliculas/src/App.js
+++ pagina-de-peliculas/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header';
