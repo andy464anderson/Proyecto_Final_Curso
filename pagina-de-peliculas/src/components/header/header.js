370cb97298537b1edd4fbd3c41a36395fc0a4f25
Jandrocr__ <jandristu7@gmail.com>
2023-06-07 14:21:11 +0200

    Revert "header responsive solucionado"
    This reverts commit ea6bee394b6cfbda89bf10aeac29a9d2cc8f056b.

--- pagina-de-peliculas/src/components/header/header.js
+++ pagina-de-peliculas/src/components/header/header.js
@@ -15,7 +15,6 @@ function Header({ children }) {
   const location = useLocation();
   const [rutaActual, setRutaActual] = useState("");
   const [abierto, setAbierto] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth); // Estado para almacenar el ancho de la ventana
 
   useEffect(() => {
     setRutaActual(location.pathname);
@@ -25,6 +24,7 @@ function Header({ children }) {
   if (userData !== undefined) {
     nombre_usuario = "/perfil/" + userData.nombre_usuario;
   }
+
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -39,18 +39,6 @@ function Header({ children }) {
       document.removeEventListener("click", handleClickOutside);
     };
   }, [showDropdown]);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []); // El segundo argumento del useEffect está vacío para que solo se ejecute una vez al montar el componente
 
   const handleLogout = (e) => {
     setShowDropdown(!showDropdown);
@@ -69,113 +57,110 @@ function Header({ children }) {
 
   const toggleMenuBurger = () => {
     var top = $("#menu-burger").css("left");
-    if (top === "0px") {
+    if(top === "0px"){
       $("#menu-burger").css({
         "left": "-200px"
       });
-    } else {
+    }else{
       $("#menu-burger").css({
         "left": "0px"
       });
     }
-  };
+  }
 
   return (
     <div>
 
-      <div>
-
+        <div>
+          
         <nav>
-          <div id="menu-burger">
-            <ul>
-              <li>
-                <Link onClick={toggleMenuBurger} to="/">Inicio</Link>
-              </li>
-              <li>
-                <Link onClick={toggleMenuBurger} to="/peliculas">Peliculas</Link>
-              </li>
-              <li>
-                <Link onClick={toggleMenuBurger} to="/mapaWeb">Mapa Web</Link>
-              </li>
-              {!isLoggedIn && (
-                <li>
-                  <Link onClick={toggleMenuBurger} to="/login">Iniciar sesión</Link>
-                </li>
-              )}
-              {!isLoggedIn && (
-                <li>
-                  <Link onClick={toggleMenuBurger} to="/registrar">Registrarse</Link>
-                </li>
-              )}
-              {isLoggedIn && (
-                <li>
-                  <Link onClick={toggleMenuBurger} to={"/perfil/" + userData.nombre_usuario}>Ver perfil</Link>
-                </li>
-              )}
-              {isLoggedIn && (
-                <li>
-                  <Link to="#" onClick={handleLogout}>Cerrar sesión</Link>
-                </li>
-              )}
-              {location.pathname !== "/search" && (
-                <li>
-                  <Link onClick={toggleMenuBurger} to="/search">Buscador</Link>
-                </li>
-              )}
-
-            </ul>
-          </div>
+        <div id="menu-burger">
+              <ul>
+                    <li>
+                      <Link onClick={toggleMenuBurger} to="/">Inicio</Link>
+                    </li>
+                    <li>
+                      <Link onClick={toggleMenuBurger} to="/peliculas">Peliculas</Link>
+                    </li>
+                    <li>
+                      <Link onClick={toggleMenuBurger} to="/mapaWeb">Mapa Web</Link>
+                    </li>
+                    {!isLoggedIn && (
+                      <li>
+                          <Link onClick={toggleMenuBurger} to="/login">Iniciar sesión</Link>
+                      </li>
+                    )}
+                    {!isLoggedIn && (
+                      <li>
+                          <Link onClick={toggleMenuBurger} to="/registrar">Registrarse</Link>
+                      </li>
+                    )}
+                    {isLoggedIn && (
+                      <li>
+                          <Link onClick={toggleMenuBurger} to={"/perfil/"+userData.nombre_usuario}>Ver perfil</Link>
+                      </li>
+                    )}
+                    {isLoggedIn && (
+                      <li>
+                          <Link to="#" onClick={handleLogout}>Cerrar sesión</Link>
+                      </li>
+                    )}
+                    <li>
+                      <Link onClick={toggleMenuBurger} to="/search">Buscador</Link>
+                    </li>
+                  </ul>
+            </div>
           <div className="menu-left">
             <img src="logo.svg" alt="Logo" />
           </div>
           <div className="menu-middle">
-            {window.innerWidth > 767 && (
-              <ul>
-                <li>
-                  <Link to="/">Inicio</Link>
+          {window.innerWidth > 767 && (
+            <ul>
+              <li>
+                <Link to="/">Inicio</Link>
+              </li>
+              <li>
+                <Link to="/peliculas">Peliculas</Link>
+              </li>
+              <li>
+                <Link to="/mapaWeb">Mapa Web</Link>
+              </li>
+              {!isLoggedIn && (
+                <li className="dropdown" onClick={toggleDropdown}>
+                  <Link href="#">Sesión</Link>
+                  <div
+                    className="dropdown-content"
+                    style={{ display: showDropdown ? "block" : "none" }}
+                  >
+                    <Link className="topDropdown" to="/login">Iniciar sesión</Link>
+                    <Link className="bottomDropdown" to="/registrar">Registrarse</Link>
+                  </div>
                 </li>
-                <li>
-                  <Link to="/peliculas">Peliculas</Link>
+              )}
+              {isLoggedIn && (
+                <li className="dropdown" onClick={toggleDropdown}>
+                  <Link href="#">Perfil</Link>
+                  <div
+                    className="dropdown-content"
+                    style={{ display: showDropdown ? "block" : "none" }}
+                  >
+                    <Link className="topDropdown" to={"/perfil/"+userData.nombre_usuario}>Ver perfil</Link>
+                    <Link className="bottomDropdown" to="#" onClick={handleLogout}>
+                      Cerrar sesión
+                    </Link>
+                  </div>
                 </li>
-                <li>
-                  <Link to="/mapaWeb">Mapa Web</Link>
-                </li>
-                {!isLoggedIn && (
-                  <li className="dropdown" onClick={toggleDropdown}>
-                    <Link href="#">Sesión</Link>
-                    <div
-                      className="dropdown-content"
-                      style={{ display: showDropdown ? "block" : "none" }}
-                    >
-                      <Link className="topDropdown" to="/login">Iniciar sesión</Link>
-                      <Link className="bottomDropdown" to="/registrar">Registrarse</Link>
-                    </div>
-                  </li>
-                )}
-                {isLoggedIn && (
-                  <li className="dropdown" onClick={toggleDropdown}>
-                    <Link href="#">Perfil</Link>
-                    <div
-                      className="dropdown-content"
-                      style={{ display: showDropdown ? "block" : "none" }}
-                    >
-                      <Link className="topDropdown" to={"/perfil/" + userData.nombre_usuario}>Ver perfil</Link>
-                      <Link className="bottomDropdown" to="#" onClick={handleLogout}>
-                        Cerrar sesión
-                      </Link>
-                    </div>
-                  </li>
-                )}
-              </ul>
+              )}
+            </ul>
             )}
           </div>
           <div className="menu-right">
-            {(windowWidth <= 767 || abierto) && (
-              <button onClick={toggleMenuBurger}>
-                <FontAwesomeIcon id="burger-icono" icon={faBars} />
-              </button>
+            {window.innerWidth <= 767 &&(
+                          <button onClick={toggleMenuBurger}>
+                          <FontAwesomeIcon id="burger-icono" icon={faBars} />
+                        </button>
             )}
-            {location.pathname !== "/search" && windowWidth > 767 && (
+            {location.pathname !== "/search" && window.innerWidth > 767 && (
               <Link to="/search">
                 <input type="text" placeholder="Buscar" />
               </Link>
@@ -183,9 +168,9 @@ function Header({ children }) {
           </div>
 
         </nav>
-      </div>
-
-
+        </div>
+        
+      
 
       <div id="separador-header">
       </div>
